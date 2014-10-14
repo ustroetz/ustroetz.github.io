a27be69f14ed5199a48044f78a040c7801c3a2c4
Ulrich Stroetz <ustroetz@gmail.com>
2014-10-14 21:49:16 +0200

    changes to bio

--- Application/js/map.js
+++ Application/js/map.js
@@ -4,7 +4,7 @@ function onEachFeature(feature, layer) {
   var type = feature.properties.type;
 
   if (type == "expierence") {
-    var popupContent = "<div><div><a href=" +  feature.properties.website + "><h3 class='popupHeading'>" + feature.properties.name + "</h3></a><p class='popupHeading'> (" + feature.properties.time + ")</p></div><p>" + feature.properties.description + "</p><p>&#9654 " + feature.properties.skills + "</p></div>"
+    var popupContent = "<div><div><a href=" +  feature.properties.website + "><h3 class='popupHeading'>" + feature.properties.name + " - " + feature.properties.position + "</h3></a><p class='popupHeading'> (" + feature.properties.time + ")</p></div><div class='extented-popup'<p>" + feature.properties.description + "</p><p>&#9654 " + feature.properties.skills + "</p></div></div>"
   }
   else if (type == "education") {
     var popupContent = "<div><div class='popupHeading'><a href=" +  feature.properties.website + "><h3 class='popupHeading'>" + feature.properties.name + "</h3></a><p class='popupHeading'> (" + feature.properties.time + ")</p></div><p>&#9654 " + feature.properties.degree + "</p></div>"
@@ -15,27 +15,35 @@ function onEachFeature(feature, layer) {
     layer.bindPopup(popupContent);
 };
 
-
 map = L.mapbox.map('map', 'uli.i535bj4f', {
     zoomControl: false
   }).setView([52.526666,13.396454], 5);
 
+var bioMarkerLayer = L.geoJson(bio, {
+     onEachFeature: onEachFeature
+ });
 
-var bioMarker = L.geoJson(bio, {
-    onEachFeature: onEachFeature
+
+
+
+bioMarkerLayer.eachLayer(function(marker) {
+  marker.on('click', function(e) {
+    $( ".extented-popup" ).show();
+  });
+  marker.setIcon(L.mapbox.marker.icon({
+          'marker-color': '#242B31'
+    }));
 }).addTo(map);
-
 
 
 var initMarkerLoop = function () {
 // Open popup when user mouses over a marker
 map.featureLayer.on('ready', function(e) {
     var markers = [];
-    bioMarker.eachLayer(function(marker) { markers.push(marker); });
+    bioMarkerLayer.eachLayer(function(marker) { markers.push(marker); });
     cycle(markers);
 
     map.on('click', function() {
-      console.log("click");
       mapInteraction = true;
     });
 });
@@ -49,11 +57,13 @@ function cycle(markers) {
       if (!mapInteraction){
       var countMarkers = markers.length;
         if (++i > countMarkers - 1) i = 0;
-        console.log(countMarkers);
-        console.log(i);
-        map.setView(markers[i].getLatLng(), 2);
+        map.panTo(markers[i].getLatLng(), {
+          animate: true,
+          duration: 2.0,
+          easeLinearity: 0.1,
+           });
         markers[i].openPopup();
-        window.setTimeout(run, 3000);
+        window.setTimeout(run, 5000);
 };
   };
     run();
